b8e4c9e61ff8a7eb6fa924a286d435dd9bd01499
toba_0x7 <israelowolabi1@gmail.com>
2023-07-23 19:57:43 +0100

    modified:   add_input_validation.py

--- add_input_validation.py
+++ add_input_validation.py
@@ -8,31 +8,32 @@ import json
 import boto3
 
 # Arguments
-parser = argparse.ArgumentParser(description="Provides translation  between one source language and another of the same set of languages.")
+# Parse command-line arguments to get the input filename containing JSON data.
+parser = argparse.ArgumentParser(description="Provides translation between one source language and another of the same set of languages.")
 parser.add_argument(
     '--file',
     dest='filename',
     help="The path to the input file. The file should be valid json",
     required=True
-    )
+)
 
 args = parser.parse_args()
 
 # Functions
 
-# Open the input file to get the json.
+# Open the input file to get the JSON data.
 def open_input():
     with open(args.filename) as file_object:
         contents = json.load(file_object)
         return contents['Input']
 
-# Boto3 function to use Amazon Translate to translate the text and only return the Translated Text
+# Boto3 function to use Amazon Translate to translate the text and only return the Translated Text.
 def translate_text(**kwargs):
     client = boto3.client('translate')
     response = client.translate_text(**kwargs)
     print(response['TranslatedText'])
 
-# Add a Loop to iterate over the json file.
+# Add a Loop to iterate over the JSON file.
 def translate_loop():
     input_text = open_input()
     for item in input_text:
@@ -41,17 +42,19 @@ def translate_loop():
         else:
             raise SystemError
 
-# Add our input validation as a function here.
+# Add input validation as a function here.
 def input_validation(item):
+    # List of valid language codes for translation.
     languages = ["af","sq","am","ar","az","bn","bs","bg","zh","zh-TW","hr","cs","da","fa-AF",
                 "nl","en","et","fi","fr","fr-CA","ka","de","el","ha","he","hi","hu","id","it",
                 "ja","ko","lv","ms","no","fa","ps","pl","pt","ro","ru","sr","sk","sl","so","es",
                 "sw","sv","tl","ta","th","tr","uk","ur","vi"
                 ]
-    json_input=item
+    json_input = item
     SourceLanguageCode = json_input['SourceLanguageCode']
     TargetLanguageCode = json_input['TargetLanguageCode']
 
+    # Perform input validation checks based on language codes.
     if SourceLanguageCode == TargetLanguageCode:
         print("The SourceLanguageCode is the same as the TargetLanguageCode - nothing to do")
         return False
@@ -71,7 +74,7 @@ def input_validation(item):
         print("There is an issue")
         return False
 
-# Main Function - use to call other functions
+# Main Function - used to call other functions
 def main():
     translate_loop()
 
